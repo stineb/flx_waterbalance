d384951e284a1041d17879ab3216477ad1e31042
Anna Grossmann <ADB@Annas-MacBook-Pro-2.local>
2025-02-18 18:04:07 +0100

    download_flx and read table in data

---
+++ data/00_download_data.R
@@ -0,0 +1,13 @@
+
+```r
+library(readr)
+flx_fullyearsequence <- read_csv("data/fdk_site_fullyearsequence.csv")
+View(flx_fullyearsequence)
+
+flx_fullyearsequence_info <- read_csv("data/fdk_site_info.csv")
+
+#cti_marthews_all <-
+
+
+
+```
