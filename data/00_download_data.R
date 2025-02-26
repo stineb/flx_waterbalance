27d65a925b416cb94f0d8721ad944712cf1bfafe
Anna Grossmann <anna.grossmann@students.unibe.ch>
2025-02-26 15:19:19 +0100

    merged cti and flx in one table of relevance

--- data/00_download_data.R
+++ data/00_download_data.R
@@ -1,11 +1,33 @@
 
 
 library(readr)
+library(dplyr)
+library(tidyverse)
+library(terra)
+install.packages("remotes")
+library(remotes)
+remotes::install_github("https://github.com/geco-bern/FluxDataKit")
+library(FluxDataKit)
+
 flx_fullyearsequence <- read_csv("data/fdk_site_fullyearsequence.csv")
 View(flx_fullyearsequence)
 
-flx_fullyearsequence_info <- read_csv("data/fdk_site_info.csv")
+#load sitenames, lon, lat, height and p_over_pet:
+flx_all_data <- read_csv("data/fdk_site_info.csv")
 
-#cti_marthews_all <-
+#Marthews netCDF data as a raster:
+cti_all_data <- rast("/data/archive/gti_marthews_2015/data/ga2.nc")
+
+#merge values from cti and flx:
+table_merged <- flx_all_data |>
+  dplyr::select(sitename, lon, lat, elv, canopy_height, whc, mat, p_over_pet) |>
+  bind_cols(
+    terra::extract(
+      cti_all_data,
+      flx_all_data |>
+        dplyr::select(lon, lat)
+    )
+  ) |>
+  dplyr::select(-ID, cti = Band1)
 
 
