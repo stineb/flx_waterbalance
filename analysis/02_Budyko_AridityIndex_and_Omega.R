9c0702b28c1fb0ab0f00f91fd923df5e5307ae41
Anna Grossmann <anna.grossmann@students.unibe.ch>
2025-03-27 20:38:23 +0100

    compute deviation of ε ​

--- analysis/02_Budyko_AridityIndex_and_Omega.R
+++ analysis/02_Budyko_AridityIndex_and_Omega.R
@@ -1,42 +1,43 @@
 
+library(dplyr)
 
-
-# Funktion zur Berechnung von ω
-solve_omega <- function(aet, p, pet) {
-  #if (is.na(aet) | is.na(p) | is.na(pet) | p == 0) {
-  #  return(NA)  # für fehlende Werte
- # }
-
-  f <- function(omega) {
-    (1 + (pet / p)^omega)^(-1 / omega) - (aet / p)
+# Funktion zur Berechnung von ω für
+solve_omega <- function(aet, p, pet) { # ω
+  fun_omega <- function(omega) {
+    (1 + (pet / p)^omega)^(-1 / omega) - (aet / p) # innere Funktion zur U msetzung der Gleichung
   }
 
   # Finde die Nullstelle im Bereich [0.01, 10]
   omega_value <- tryCatch(
-    uniroot(f, interval = c(0.01, 10))$root,
+    uniroot(fun_omega, interval = c(0.01, 10))$root,
     error = function(e) NA  # Falls uniroot fehlschlägt, gib NA zurück
   )
 
   return(omega_value)
 }
 
-budyko_fun <- function(pet, p, omega) {
-  (1 + (pet / p)^omega)^(-1 / omega)
+#estimate how much much P will be lost as actual ET
+#compute predicted ε0 (modelled / theoretical Index)
+compute_evap_index <- function(pet, p, omega) {
+  (1 + (pet / p)^omega)^(-1 / omega) # eg. ε0 (predicted EvapoINdex)
+
 }
 
+test = 3
 # Berechnung von ω für jede Zeile der Tabelle
 table_budyko <- site_annual_mean |>
   mutate(
-    omega = mapply(solve_omega, AET_mean, P_mean, PET_mean)
-  )|>
-  mutate(
-    AET_over_P = mapply(budyko_fun, PET_mean, P_mean, omega),
-  ) |>
+    omega = mapply(solve_omega, AET_mean, P_mean, PET_mean), #
+    Evap_Index_predicted = mapply(compute_evap_index, PET_mean, P_mean, omega), #predict of ε0
+    Evap_Index_observed = AET_mean / P_mean,
+    epsilon_deviation = Evap_Index_observed - Evap_Index_predicted
+    ) |>
   left_join(
     table_merged |> select(sitename, cti),  # Only add CTI and Elevation
     by = "sitename"
   )
 
+head(table_budyko)
 
 # what site characteristics are statistically associated with omega.
 
