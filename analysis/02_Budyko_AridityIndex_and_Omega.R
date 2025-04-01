67a152cc5233cce2ba0be0d8364325923ce95e7c
Anna Grossmann <anna.grossmann@students.unibe.ch>
2025-04-01 15:28:14 +0200

    - compute epsilon and cti correlation and regression. - plot cti relations

--- analysis/02_Budyko_AridityIndex_and_Omega.R
+++ analysis/02_Budyko_AridityIndex_and_Omega.R
@@ -23,7 +23,7 @@ compute_evap_index <- function(pet, p, omega) {
 
 }
 
-test = 3
+
 # Berechnung von ω für jede Zeile der Tabelle
 table_budyko <- site_annual_mean |>
   mutate(
@@ -39,8 +39,58 @@ table_budyko <- site_annual_mean |>
 
 head(table_budyko)
 
+
+#CORRELATION----------------------
 # what site characteristics are statistically associated with omega.
+# I want to figure out, how CTI and deviation are correlating
+library(dplyr)
+clean_budyko <- table_budyko |>
+  filter(!is.na(cti) & !is.na(epsilon_deviation))
+
+cor(clean_budyko$cti,clean_budyko$epsilon_deviation, method = 'pearson')
+plot(clean_budyko$cti, clean_budyko$epsilon_deviation, main = 'Korrelation')
+# results with -0.02786773 is very low and negatively significantly under the threshold
+# correlation is therefore given
+# threshold was set to 0.05
 
 
- # I want to figure out, how CTI and Omega are correlating
+
+#REGRESSION----------------------
+regression_cti <- clean_budyko |>
+  lm(epsilon_deviation ~ cti)
+
+head(lm_model)
+
+
+
+#plot the relations between cti and epsilon deviations
+library(ggplot2)
+install.packages("labeling")
+install.packages('farver')
+
+gg_deviation_per_site <- table_budyko |>
+  ggplot(
+    na.omit(table_budyko), aes(x = sitename, y = epsilon_deviation)) +
+  geom_boxplot() +
+  labs(title = "Deviation from Budyko (ε') at different sites",
+       x = "Sites",
+       y = "Deviations ε' ") +
+  #theme_minimal()
+
+ggsave(here::here("~/flx_waterbalance/data/epsilon_deviation_budyko.png"), width = 8, height = 5)
+
+
+
+ggplot(na.omit(table_budyko), aes(x = cti, y = epsilon_deviation)) +
+  geom_point() +
+  geom_smooth(method = "lm", col = "blue") +
+  labs(title = "Relationship between CTI and ε'",
+       x = "Compound Topographic Index (CTI)",
+       y = "Deviation from Budyko Curve (ε')") +
+  theme_minimal()
+  ggsave(here::here("~/flx_waterbalance/data/cti_budyko_relation_regression.png"), width = 6, height = 3.5)
+
+
+
+
 
